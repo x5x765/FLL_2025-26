c0e7d8adf03d810afede4926bc3b1389d9f84952
x5x765 <oyer03@outlook.com>
2025-11-13 17:10:53 -0600

    Added dragging and background music

--- python/main.py
+++ python/main.py
@@ -1,17 +1,28 @@
-import pygame
+import pygame as pg
 from objects import *
-pygame.init()
+pg.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Artifact Piece")
-clock = pygame.time.Clock()
+pg.mixer.init()
+pg.mixer.music.load(r"sounds\background.mp3")
+pg.mixer.music.play(-1, 0.0)
 
+screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+pg.display.set_caption("Artifact Piece")
+clock = pg.time.Clock()
+piece = Piece(200, 200, r"images\piece1.png")
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    piece.update(screen)
+    screen.fill((230, 230, 230))
+    piece.draw(screen)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-        elif event.type == pygame.mouse.get_pressed():
-            pos = pygame.mouse.get_pos()
-            if :
-
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if piece.rect.collidepoint(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]):
+                piece.isSelected = not piece.isSelected
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
+    pg.display.flip()
+    clock.tick(60)
