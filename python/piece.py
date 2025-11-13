9ff4287c234afb81aba2592a81d7d1d9c4d33c35
x5x765 <oyer03@outlook.com>
2025-11-13 17:10:53 -0600

    Added dragging and background music

--- python/piece.py
+++ python/piece.py
@@ -2,9 +2,19 @@ import pygame
 pygame.init()
 
 class Piece:
-    def __init__(self,x,y, image_path):
+    def __init__(self, x : int, y : int, image_path):
         self.x = x
         self.y = y
-        self.image = pygame.image.load(image_path)
-    def draw(self,screen):
-        pass
+        self.image = pygame.image.load(image_path).convert()
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey((255, 255, 255))
+        self.isSelected = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self, screen):
+        if self.isSelected:
+            screen.blit(self.image, self.rect)
+            self.rect.center = pygame.mouse.get_pos()
+
