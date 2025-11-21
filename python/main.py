32598103fed9416ac4b3c820a990735f84f37fb7
x5x765 <oyer03@outlook.com>
2025-11-21 16:33:42 -0600

    Finished! Added actual snap coordinates, a few gaps remaining.

--- python/main.py
+++ python/main.py
@@ -9,22 +9,22 @@ pg.mixer.music.play(-1, 0.0)
 screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 pg.display.set_caption("Artifact Piece")
 clock = pg.time.Clock()
-piece1 = Piece(200, 200, r"images\piece (1).png")
-piece2 = Piece(200, 200, r"images\piece (2).png")
-piece3 = Piece(200, 200, r"images\piece (3).png")
-piece4 = Piece(200, 200, r"images\piece (4).png")
-piece5 = Piece(200, 200, r"images\piece (5).png")
-piece6 = Piece(200, 200, r"images\piece (6).png")
-piece7 = Piece(200, 200, r"images\piece (7).png")
-piece8 = Piece(200, 200, r"images\piece (8).png")
-piece9 = Piece(200, 200, r"images\piece (9).png")
-piece10 = Piece(200, 200, r"images\piece (10).png")
-piece11 = Piece(200, 200, r"images\piece (11).png")
-piece12 = Piece(200, 200, r"images\piece (12).png")
-piece13 = Piece(200, 200, r"images\piece (13).png")
-piece14 = Piece(200, 200, r"images\piece (14).png")
-piece15 = Piece(200, 200, r"images\piece (15).png")
-base_pot = Piece(200, 200, r"images\base_pot.png")
+piece1 = Piece(666, 150, r"images\piece (1).png")
+piece2 = Piece(922, 151, r"images\piece (2).png")
+piece3 = Piece(631, 258, r"images\piece (3).png")
+piece4 = Piece(778, 202, r"images\piece (4).png")
+piece5 = Piece(989, 253, r"images\piece (5).png")
+piece6 = Piece(902, 277, r"images\piece (6).png")
+piece7 = Piece(763, 294, r"images\piece (7).png")
+piece8 = Piece(676, 392, r"images\piece (8).png")
+piece9 = Piece(932, 414, r"images\piece (9).png")
+piece10 = Piece(646, 537, r"images\piece (10).png")
+piece11 = Piece(943, 513, r"images\piece (11).png")
+piece12 = Piece(868, 534, r"images\piece (12).png")
+piece13 = Piece(656, 674, r"images\piece (13).png")
+piece14 = Piece(780, 648, r"images\piece (14).png")
+piece15 = Piece(895, 674, r"images\piece (15).png")
+base_pot = Piece(800, 425, r"images\base_pot_cracked.png")
 
 pieces = [piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9, piece10, piece11, piece12, piece13, piece14, piece15]
 
@@ -46,5 +46,12 @@ while running:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 running = False
+            elif event.key == pg.K_s: # for debug
+                save = open("coords.txt", "wt")
+                for piece in pieces:
+                    save.write(str(piece) + "\n")
+                save.close()
+                del save
+
     pg.display.flip()
     clock.tick(60)
--- python/piece.py
+++ python/piece.py
@@ -4,24 +4,25 @@ pygame.init()
 
 class Piece:
     def __init__(self, xGoal, yGoal, image_path):
-        self.x = randint(50, 1500)
-        self.y = randint(50, 800)
         self.xGoal = xGoal
         self.yGoal = yGoal
         self.image = pygame.image.load(image_path).convert()
-        self.rect = self.image.get_rect()
         self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
         self.isSelected = False
-        self.rect.center = (self.x, self.y)
+        self.rect.center = (randint(50, 1500), randint(50, 800))
         self.isInPlace = False
+        self.imagePath = image_path
+
+    def __str__(self):
+        return f"{str(self.rect.centerx)}, {str(self.rect.centery)}, {self.imagePath}"
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def update(self, screen):
         if self.isSelected and not self.isInPlace:
-            screen.blit(self.image, self.rect)
             self.rect.center = pygame.mouse.get_pos()
-        if self.rect.centerx in range(self.xGoal-10, self.xGoal+10):
+        if self.rect.centerx in range(self.xGoal-10, self.xGoal+10) and self.rect.centery in range(self.yGoal-10, self.yGoal+10) and not self.isInPlace:
             self.rect.center = (self.xGoal, self.yGoal)
             self.isInPlace = True
